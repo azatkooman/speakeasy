89adf55d828364e4189875eb9349750741f494f5
Azat Kooman <azatkooman@gmail.com>
2025-12-21 20:29:54 +0500

    SpeakEasy Update Dec 21 2025 20:29

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.speakeasy',
+  appId: 'com.azatkooman.speakeasy',
   appName: 'SpeakEasy AAC',
   webDir: 'dist'
 };
--- node_modules/@capgo/capacitor-speech-synthesis/ios/Sources/SpeechSynthesisPlugin/SpeechSynthesisPlugin.swift
+++ node_modules/@capgo/capacitor-speech-synthesis/ios/Sources/SpeechSynthesisPlugin/SpeechSynthesisPlugin.swift
@@ -7,7 +7,7 @@ import AVFoundation
  */
 @objc(SpeechSynthesisPlugin)
 public class SpeechSynthesisPlugin: CAPPlugin, CAPBridgedPlugin, AVSpeechSynthesizerDelegate {
-    private let pluginVersion: String = "8.0.5"
+    private let pluginVersion: String = "8.0.6"
     public let identifier = "SpeechSynthesisPlugin"
     public let jsName = "SpeechSynthesis"
     public let pluginMethods: [CAPPluginMethod] = [
