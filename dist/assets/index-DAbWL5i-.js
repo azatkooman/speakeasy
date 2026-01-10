35f1460fe567982595fbd5696940feb053860735
Azat Kooman <azatkooman@gmail.com>
2026-01-10 12:40:36 +0500

    SpeakEasy Update Jan 11 2026 12:40

---
+++ dist/assets/index-DAbWL5i-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D0pI9zJh.js","assets/index-DBe0v9AN.js"])))=>i.map(i=>d[i]);
+import{r as t,_ as s}from"./index-DBe0v9AN.js";const r=t("SpeechSynthesis",{web:()=>s(()=>import("./web-D0pI9zJh.js"),__vite__mapDeps([0,1])).then(e=>new e.SpeechSynthesisWeb)});export{r as SpeechSynthesis};
